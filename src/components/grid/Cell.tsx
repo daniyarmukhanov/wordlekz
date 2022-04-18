92bb560aeb6382a5ee084c418b471c97049e599a
Ilyas <ilyas@uxstone.co>
2022-04-18 09:31:22 +0600

    style(Grid): changed the cell sizes

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Cell = ({ value, status }: Props) => {
   const classes = classnames(
-    'w-12 h-12 lg:w-14 lg:h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-lg font-bold rounded',
+    'w-[44px] h-[44px] sm:w-[40px] sm:h-[40px] lg:w-14 lg:h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-lg font-bold rounded',
     {
       'bg-white border-slate-200': !status,
       'bg-slate-400 text-white border-slate-400': status === 'absent',
